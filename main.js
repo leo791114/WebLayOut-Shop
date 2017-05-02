a815e2ea731209e00e0d5a9898f3760edaabc140
leo791114 <leo791114@hotmail.com>
2017-05-03 02:31:59 +0800

    upto sm and xs load more part

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ $(function () {
     hoverOpen();
     emailFocus();
     tabToggle();
-})
+    load();
+    less();
+    resizeInit();
+});
 
 function hoverOpen() {
     $('.dropdown').hover(function () {
@@ -33,5 +36,67 @@ function tabToggle() {
 
     $('#previous').on('click', function () {
         $('.nav-tabs > li.active').prev('li').find('a').trigger('click');
+    });
+}
+
+// Load and Less function
+function load() {
+    // $('#clearance_commodity .row>div').hide();
+    if ($(window).width() >= 992) {
+        $('#clearance_commodity .row>div').slice(0, 4).show();
+        $('#load').on('click', function (event) {
+            event.preventDefault();
+            $('#clearance_commodity .row>div:hidden').slice(0, 4).slideDown(1500);
+            $('body').animate({
+                    scrollTop: $(this).offset().top
+                },
+                1500);
+            $('#clearance_commodity').append($(window).width());
+        });
+    } else if ($(window).width() >= 768 && $(window).width() < 992) {
+        $('#clearance_commodity .row>div').slice(0, 3).show();
+        $('#load').on('click', function (event) {
+            event.preventDefault();
+            $('#clearance_commodity .row>div:hidden').slice(0, 3).slideDown(1500);
+            $('html,body').animate({
+                    scrollTop: $(this).offest().top
+                },
+                1500);
+            $('#clearance_commodity').append($(window).width());
+        });
+    } else {
+        $('#clearance_commodity .row>div').slice(0, 2).show();
+        $('#load').on('click', function () {
+            event.preventDefault();
+            $('#clearance_commodity .row>div:hidden').slice(0, 2).slideDown(1500);
+            $('body').animate({
+                    scrollTop: $(this).offest().top
+                },
+                1500);
+        });
+    }
+}
+
+
+function less() {
+    $('#less').on('click', function () {
+        if ($(window).width() > 992) {
+            var $show_items = $('#clearance_commodity .row>div').not('#clearance_commodity .row>div:hidden').length;
+            if ($show_items > 4) {
+                $('#clearance_commodity .row>div').slice($show_items - 4, $show_items).slideUp(1500);
+            }
+        }
+    });
+}
+
+
+function resizeInit() {
+    $(window).resize(function () {
+        $('#clearance_commodity .row>div').hide();
+        if ($(window).width() >= 992) {
+            $('#clearance_commodity .row>div').slice(0, 4).show();
+        } else if ($(window).width() < 992 && $(window).width() > 768) {
+            $('#clearance_commodity .row>div').slide(0, 3).show();
+        }
     });
 }
