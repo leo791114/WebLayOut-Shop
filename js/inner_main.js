5765439df283611f58a9a39f0037f591f5bbaa36
leo791114 <leo791114@hotmail.com>
2017-06-08 19:59:37 +0800

    dynamic pagination via bootpag

--- js/inner_main.js
+++ js/inner_main.js
@@ -1,5 +1,7 @@
 $(function () {
     selectDisplay();
+    tabNavigateEffect();
+    nextPrevNavigation();
 });
 
 function selectDisplay() {
@@ -12,3 +14,17 @@ function selectDisplay() {
     });
 }
 
+function tabNavigateEffect() {
+    // Page navigate
+    $('#product #pagination ul li:not(#next)').on('click', function () {
+        $('#product #pagination ul li.active').removeClass('active');
+        $(this).addClass('active');
+    });
+}
+
+function nextPrevNavigation() {
+    $('#product #pagination ul li#next').on('click', function () {
+        $('#pagination ul > li.active').removeClass('active').next('li').addClass('active').find('a').trigger('click');
+    });
+
+}
