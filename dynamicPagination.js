9dec77fbfeed8399bc9e22e80862e7fa89d0c9e0
leo791114 <leo791114@hotmail.com>
2017-06-19 20:34:35 +0800

    dynamic pagination

--- dynamicPagination.js
+++ dynamicPagination.js
@@ -70,7 +70,7 @@
 
             // make current page active
             $('ul.' + settings.ulClass + ' li[value="' + settings.page +
-                    '"]:not( .' + settings.firstClass + ', .' + settings.prevClass + ', .' + settings.nextClass + ', .' + settings.lastClass + ')')
+                '"]:not( .' + settings.firstClass + ', .' + settings.prevClass + ', .' + settings.nextClass + ', .' + settings.lastClass + ')')
                 .addClass('active');
 
             // find ul with pagination class
@@ -82,7 +82,7 @@
                 if ($this.hasClass(settings.disabledClass) || $this.hasClass(settings.activeClass)) {
                     return;
                 }
-            })
+            });
         }
 
         function href(value) {
